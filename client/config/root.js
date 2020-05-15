a22341707a323758b1c5b8e79071de1bc4db972b
Andrey <kochmuradov@gmail.com>
2020-05-15 16:38:22 +0300

    Project Pipec7

--- client/config/root.js
+++ client/config/root.js
@@ -84,9 +84,7 @@ export default (props) => {
           <Switch>
             <Route exact path="/" component={() => <DummyView />} />
             <Route exact path="/planets/:planetId" component={() => <DummyView />} />
-            <Route exact path="/dashboard" component={() => <Home />} />
-            <Route exact path='/dashboard/main' component={()=><Home />} />
-            <Route exact path='/dashboard/profile/:username' component={()=><Home />} />
+            <Route exact path="/dashboard*" component={() => <Home />} />
             <PrivateRouteConnected exact path="/hidden-route" component={() => <DummyView />} />
             <Route component={() => <NotFound />} />
           </Switch>
